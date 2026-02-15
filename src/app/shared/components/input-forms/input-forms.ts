1869e384908acd9ab4b493723bd538d690865f8d
Nattapong Promthong <banker.z412@gmail.com>
2026-02-15 22:26:23 +0700

    feat: create input forms components

---
+++ src/app/shared/components/input-forms/input-forms.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'git101-input-forms',
+  imports: [],
+  templateUrl: './input-forms.html',
+  styleUrl: './input-forms.css',
+})
+export class InputForms {
+
+}
