a02f9bd1f44cc79b7bced1617ee7930b2ab34262
Nattapong Promthong <banker.z412@gmail.com>
2026-03-22 17:13:40 +0700

    feat: create select component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Card } from './shared/components/card/card';
 import { RadioForms } from './shared/components/radio-forms/radio-forms';
+import { Select } from './shared/components/select/select';
 
 @Component({
   selector: 'git101-root',
-  imports: [RouterOutlet, Card, RadioForms],
+  imports: [RouterOutlet, Card, RadioForms, Select],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/shared/components/select/select.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'git101-select',
+  imports: [],
+  templateUrl: './select.html',
+  styleUrl: './select.css',
+})
+export class Select {
+
+}
