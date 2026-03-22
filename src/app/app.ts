43bf4e21b2a6c8092046011e287fc338fde5b12c
Nattapong Promthong <banker.z412@gmail.com>
2026-03-22 17:34:59 +0700

    feat: change name

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,17 @@ import { RouterOutlet } from '@angular/router';
   selector: 'git101-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('git101');
+  protected readonly name = 'Bank Nattapong';
+
+  getTitle() {
+    return this.title;
+  }
+
+  getName() {
+    return this.name;
+  }
 }
